85af34d0d2e5c5357b8872e390c1d252e531dc62
Manfred Schreiber <manfred.schreiber@jambit.com>
2017-01-02 14:50:48 +0100

    .

--- build.sh
+++ build.sh
@@ -3,9 +3,8 @@
 mkdir build
 VERSION=$(git name-rev --tags --name-only $(git rev-parse HEAD))
 GITHASH=$(git log -n1 --pretty='%h')
-rm build/darwin_amd64.zip
-rm build/linux_amd64.zip
-rm build/linux_i386.zip
+BINNAME="rcmd"
+rm build/*.zip
 
 rm -rf build/darwin
 rm -rf build/linux
@@ -18,10 +17,10 @@ echo VERSION: $VERSION
 echo GITHASH: $GITHASH
 cd build/darwin/amd64
 env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$VERSION -X main.githash=$GITHASH" ../../../
-zip ../../darwin_amd64.zip rcmd
+zip ../../${BINNAME}_darwin_amd64.zip $BINNAME
 cd ../../linux/amd64
 env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$VERSION -X main.githash=$GITHASH" ../../../
-zip ../../linux_amd64.zip rcmd
+zip ../../${BINNAME}_linux_amd64.zip $BINNAME
 cd ../i386
 env GOOS=linux GOARCH=386 go build -ldflags "-X main.version=$VERSION -X main.githash=$GITHASH" ../../../
-zip ../../linux_i386.zip rcmd
+zip ../../${BINNAME}_linux_i386.zip $BINNAME
